4681bd83a1ab6c38b77e6dc0f4757facf83cbb05
jg <jjtjs159@gmail.com>
2020-08-22 22:40:40 +0900

    컴포넌트, 스타일 분리

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import styles from './Home.style';
 
 const Home = () => {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text style={styles.text}>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#efefef',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
-
 
 export default Home;

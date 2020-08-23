8b7526cc370af87dc8acbb3e111df318c5def655
jg <jjtjs159@gmail.com>
2020-08-23 22:59:33 +0900

    babel, tsconfig, module-resolver 설정

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,25 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        // plugins: [
-        //     [
-        //         "auto-import", {
-        //             "declarations": [
-        //                 { "default": "React", "path": "react" }
-        //             ]
-        //         }
-        //     ]
-        // ]
+        plugins: [
+            // [
+            //     "auto-import", {
+            //         "declarations": [
+            //             { "default": "React", "path": "react" }
+            //         ]
+            //     }
+            // ]
+            [
+                "module-resolver", {
+                    "extensions": [
+                        ".js", "tsx", "ts",
+                    ],
+                    "root": ["."],
+                    "alias": {
+                        "components": "./src/components"
+                    }
+                }
+            ]
+        ]
     };
 };
